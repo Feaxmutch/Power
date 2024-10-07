34bfc3f86f1f9ccb3d98e9e0c5c872562b0c6daf
Feaxmutch <feaxmutch@gmail.com>
2024-10-07 13:41:02 +0300

    Copyed old solution

--- Power/Program.cs
+++ Power/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            
+            Random random = new Random();
+            int minRandom = 1;
+            int maxRandom = 100;
+            int firstNumber = 2;
+            int firstNumberInPower = firstNumber;
+            int powerCount = 1;
+            int secondNumber = random.Next(minRandom, maxRandom + 1);
+
+            while (firstNumberInPower <= secondNumber)
+            {
+                firstNumberInPower *= firstNumber;
+                powerCount++;
+            }
+
+            Console.WriteLine($"число {secondNumber} меньше числа {firstNumber} в {powerCount} степени, что равняется: {firstNumberInPower}");
+            Console.ReadKey();
         }
     }
 }
